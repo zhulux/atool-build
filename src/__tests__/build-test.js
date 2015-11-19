03dd49906bb7744c416e76b4e4b6e9b052201c33
sorrycc <sorrycc@gmail.com>
2015-11-19 21:45:52 +0800

    code style

--- src/__tests__/build-test.js
+++ src/__tests__/build-test.js
@@ -39,19 +39,15 @@ function testBuild(args, fixture) {
 }
 
 describe('lib/build', () => {
-
   pit('should build normally', () => {
     return testBuild({hash:true}, 'build-normal');
   });
-
   pit('should support decorator', () => {
     return testBuild({}, 'build-decorator');
   });
-
   pit('should support add-module-exports', () => {
     return testBuild({}, 'build-add-module-exports');
   });
-
   pit('should support jsx', () => {
     return testBuild({}, 'build-jsx');
   });
