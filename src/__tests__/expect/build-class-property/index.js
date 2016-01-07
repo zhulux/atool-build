00c820681179f223b9658788e68a7e68354e73d1
玄寂 <xuanji.jw@taobao.com>
2016-01-07 17:55:28 +0800

    fix: test fail

--- src/__tests__/expect/build-class-property/index.js
+++ src/__tests__/expect/build-class-property/index.js
@@ -4,11 +4,11 @@ webpackJsonp([1,0],[
 
 	'use strict';
 
-	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-	var A = (function () {
+	var A = function () {
 	  function A() {
 	    _classCallCheck(this, A);
 	  }
@@ -21,7 +21,7 @@ webpackJsonp([1,0],[
 	  }]);
 
 	  return A;
-	})();
+	}();
 
 	A.propTypes = 1;
 
