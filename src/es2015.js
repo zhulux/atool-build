756de105fd5bac634f606b1be03f8cf147d79b9c
sorrycc <sorrycc@gmail.com>
2016-04-22 16:14:18 +0800

    Revert "fix inherit-ie10 bug"
    This reverts commit 9e2a73eab1af4c444293144caaf1dcfbbaf7aa11.

--- src/es2015.js
+++
@@ -1,6 +0,0 @@
-import modifyBabelPreset from 'modify-babel-preset';
-export default modifyBabelPreset('babel-preset-es2015', {
-  'babel-plugin-transform-es2015-classes': {
-    loose: true,
-  },
-});
