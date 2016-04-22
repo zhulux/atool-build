2952f9d7c04c58a5fa67232cf167f04c0a752f0b
zinkey <yaya@uloveit.com.cn>
2016-04-22 11:22:44 +0800

    fix test case

--- test/expect/build-class-property/index.js
+++ test/expect/build-class-property/index.js
@@ -4,8 +4,6 @@ webpackJsonp([1,0],[
 
 	'use strict';
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 	var _class, _temp;
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -15,12 +13,9 @@ webpackJsonp([1,0],[
 	    _classCallCheck(this, A);
 	  }
 
-	  _createClass(A, [{
-	    key: 'foo',
-	    value: function foo() {
-	      console.log('foo');
-	    }
-	  }]);
+	  A.prototype.foo = function foo() {
+	    console.log('foo');
+	  };
 
 	  return A;
 	}(), _class.propTypes = 1, _temp);
