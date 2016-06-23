209469341c8d186050844a280302607c2a981f13
xuanji.jw <xuanji.jw@taobao.com>
2016-06-23 17:49:46 +0800

    fix: test error

--- test/expect/build-typescript/index.js
+++ test/expect/build-typescript/index.js
@@ -2,17 +2,24 @@ webpackJsonp([1,0],[
 /* 0 */
 /***/ function(module, exports) {
 
-	"use strict";
+	'use strict';
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	var Greeter = function () {
 	    function Greeter(greeting) {
+	        _classCallCheck(this, Greeter);
+
 	        this.greeting = greeting;
 	    }
-	    Greeter.prototype.greet = function () {
-	        return "<h1>" + this.greeting + "</h1>";
+
+	    Greeter.prototype.greet = function greet() {
+	        return '<h1>' + this.greeting + '</h1>';
 	    };
+
 	    return Greeter;
 	}();
+
 	var greeter = new Greeter('Hello, world!');
 	document.body.innerHTML = greeter.greet();
 
