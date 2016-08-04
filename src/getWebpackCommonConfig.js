38c377b7d09a5960ac4656497aa2e17fa2b8520a
xuanji.jw <xuanji.jw@taobao.com>
2016-08-04 17:50:01 +0800

    enhance autoprefix for mobile, Close #190

--- src/getWebpackCommonConfig.js
+++ src/getWebpackCommonConfig.js
@@ -150,7 +150,7 @@ export default function getWebpackCommonConfig(args) {
     postcss: [
       rucksack(),
       autoprefixer({
-        browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8'],
+        browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4'],
       }),
     ],
 
